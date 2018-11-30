3d50a18b9ae962017d6274bed3debc88e10e5c90
Chateux1 <m.satas@gmail.com>
2018-11-30 16:31:26 +0200

    add password and consent validation

--- js/validation.js
+++ js/validation.js
@@ -6,6 +6,9 @@
 // });
 
 var email = document.getElementById("email");
+var psw = document.getElementById("password");
+var pswRepeat = document.getElementById("password-repeat");
+var consent = document.getElementById("consent");
 
 email.addEventListener(
   "invalid",
@@ -23,6 +26,62 @@ email.addEventListener(
     if (!email.validity.valid) {
       email.setCustomValidity(
         "Netinkamas elektroninio pašto adreso formatas. Pavyzdys: adresas@svetaine.lt"
+      );
+    }
+  },
+  false
+);
+
+psw.addEventListener(
+  "invalid",
+  function() {
+    if (psw.validity.valueMissing) {
+      psw.setCustomValidity("Privaloma užpildyti šį lauką.");
+    }
+  },
+  false
+);
+
+psw.addEventListener(
+  "input",
+  function(e) {
+    if (!psw.validity.valid) {
+      psw.setCustomValidity(
+        "Slaptažodis turi būti: bent 8 simbolių ilgio, turėti bent po vieną iš skaičių, didžiųjų ir mažųjų raidžių."
+      );
+    }
+  },
+  false
+);
+
+pswRepeat.addEventListener(
+  "invalid",
+  function() {
+    if (pswRepeat.validity.valueMissing) {
+      pswRepeat.setCustomValidity("Privaloma užpildyti šį lauką.");
+    }
+  },
+  false
+);
+
+pswRepeat.addEventListener(
+  "input",
+  function(e) {
+    if (!pswRepeat.validity.valid) {
+      pswRepeat.setCustomValidity(
+        "Slaptažodis turi būti: bent 8 simbolių ilgio, turėti bent po vieną iš skaičių, didžiųjų ir mažųjų raidžių."
+      );
+    }
+  },
+  false
+);
+
+consent.addEventListener(
+  "invalid",
+  function() {
+    if (consent.validity.valueMissing) {
+      consent.setCustomValidity(
+        "Privaloma pažymėti, kad sutinkate su taisyklėmis."
       );
     }
   },
